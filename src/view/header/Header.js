33dbfe18ea2bd70fdb54c4119b61b9f3b68eb67a
Chonticha Chang <ruttikar_maya@hotmail.com>
2018-08-23 20:31:20 +0700

    Update className

--- src/view/header/Header.js
+++ src/view/header/Header.js
@@ -7,7 +7,7 @@ type Props = {
 export class Header extends PureComponent<Props> {
   render() {
     return (
-      <nav className="navbar" role="navigation" aria-label="main navigation">
+      <nav className="navbar header" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
           <a className="navbar-item" href="https://bulma.io">
             <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28"/>
