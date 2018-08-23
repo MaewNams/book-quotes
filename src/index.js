8da00bd9cab9c6f87f48f33eddea840ba7aa6a7d
Chonticha Chang <ruttikar_maya@hotmail.com>
2018-08-23 20:30:59 +0700

    Update main js, remove App , using RouterProvider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import {RouteProvider} from 'react-router5'
+import './index.css'
+import {store} from './state/store'
+import {router} from './router'
+import RootView from './view/Root'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+router.start()
+
+ReactDOM.render((
+  <Provider store={store}>
+    <RouteProvider router={router}>
+      <RootView />
+    </RouteProvider>
+  </Provider>
+), document.getElementById('root'))
+registerServiceWorker()
