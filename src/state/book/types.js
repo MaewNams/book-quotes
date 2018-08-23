679ffe9da7f2790d1f5756b03df54954541d7cdd
Chonticha Chang <ruttikar_maya@hotmail.com>
2018-08-23 20:22:23 +0700

    Add type Url, Character, Writer, Book, Quote

--- src/state/book/types.js
+++ src/state/book/types.js
@@ -1,5 +1,35 @@
 // @flow
 
+export type Url = string
+
+export type Character = {
+  name: string,
+  surname: string,
+  nickName: string,
+  pic: Url,
+}
+
+export type Writer = {
+  name: string,
+  pic: Url
+}
+
+export type Book = {
+  name: string,
+  writer: Writer,
+  character: Array<Character>,
+  pic: Url,
+}
+
+export type Quote = {
+  book: Book,
+  character: Character,
+  sentence: string,
+  page: number,
+}
+
+
+
 export type BookState = {
 
 }
