ad4102d50a1af0e3698688152c6533b53d25454b
Chonticha Chang <ruttikar_maya@hotmail.com>
2018-08-23 20:31:11 +0700

    Create Home

---
+++ src/view/home/Home.js
@@ -0,0 +1,17 @@
+// @flow
+import React, { PureComponent } from 'react'
+type Props = {
+
+}
+
+export class Home extends PureComponent<Props> {
+  render() {
+    return (
+     <div className="content">
+       This is Home
+     </div>
+    )
+  }
+}
+
+export default Home
