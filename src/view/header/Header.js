ae970b2d411d9df5c2bf9abfe0d67c86f1d7f191
Chonticha Chang <ruttikar_maya@hotmail.com>
2018-08-23 20:23:10 +0700

    Create Header with its story

---
+++ src/view/header/Header.js
@@ -0,0 +1,27 @@
+// @flow
+import React, { PureComponent } from 'react'
+type Props = {
+  // signIn: Function,
+}
+
+export class Header extends PureComponent<Props> {
+  render() {
+    return (
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
+            <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28"/>
+          </a>
+
+          <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+    </nav>
+    )
+  }
+}
+
+export default Header
