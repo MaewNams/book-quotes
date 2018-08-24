a44476150f302e8853b3717fb2ba3c8e37bc2649
Chonticha Chang <ruttikar_maya@hotmail.com>
2018-08-24 13:40:13 +0700

    Add connect , CardList, bookSelectors

--- src/view/home/Home.js
+++ src/view/home/Home.js
@@ -1,17 +1,44 @@
 // @flow
-import React, { PureComponent } from 'react'
-type Props = {
+import type {ApplicationState} from '../../state/types'
+import type {
+  Book,
+  Character,
+  Writer,
+  Quote
+} from '../../state/book/types'
 
+import React, { PureComponent } from 'react'
+import {connect} from 'react-redux'
+
+import {bookSelectors} from '../../state/book/selectors'
+import {CardList} from '../card'
+
+type Props = {
+  quoteList: Array<Quote>
 }
 
 export class Home extends PureComponent<Props> {
   render() {
+    const {quoteList} = this.props
     return (
      <div className="content">
-       This is Home
+       { quoteList && (
+         <CardList quoteList={quoteList} />
+       )}
      </div>
     )
   }
 }
 
-export default Home
+
+const mapStateToProps = (state: ApplicationState, props: Props) => {
+  return {
+    quoteList: bookSelectors.getQuoteList(state)
+  }
+}
+
+const withStore = connect(mapStateToProps, {
+
+})
+
+export default withStore(Home)
