e18a44d71df751ffceaae63224d9a9b30344928d
Chonticha Chang <ruttikar_maya@hotmail.com>
2018-08-24 12:56:36 +0700

    Import styled-components

--- src/view/header/Header.js
+++ src/view/header/Header.js
@@ -1,5 +1,12 @@
 // @flow
 import React, { PureComponent } from 'react'
+import styled from 'styled-components' 
+
+const NavButton = styled.a `
+  margin-right: auto;
+  margin-left: unset;
+`
+
 type Props = {
   // signIn: Function,
 }
@@ -9,19 +16,18 @@ export class Header extends PureComponent<Props> {
     return (
       <nav className="navbar header" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
+        <NavButton role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </NavButton>
           <a className="navbar-item" href="https://bulma.io">
             <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28"/>
-          </a>
-
-          <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
           </a>
         </div>
     </nav>
     )
-  }
+  } 
 }
 
 export default Header
